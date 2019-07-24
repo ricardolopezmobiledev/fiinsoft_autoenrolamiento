1e9746f1eebe3c1aa22b9fdfa1403a6839f42473
Ricardo <car.ric.lop@gmail.com>
2019-07-24 18:29:05 -0500

    commit 2 pantallas maquetado
    login y main activity

--- lib/main.dart
+++ lib/main.dart
@@ -64,39 +64,39 @@ class _MyHomePageState extends State<MyHomePage> {
         image: DecorationImage(image: AssetImage("assets/splash.jpg"), fit: BoxFit.cover),
       ),
       child:  new Container(
-        child: Column(children: <Widget>[
-          new Expanded(
-            child: new Container(
-              child: Row(
-                children: [
-                  new Expanded(
-                    child: new Container(
-                      child: null,
+          child: Column(children: <Widget>[
+            new Expanded(
+              child: new Container(
+                child: Row(
+                  children: [
+                    new Expanded(
+                      child: new Container(
+                        child: null,
+                      ),
+                      flex: 2,
                     ),
-                    flex: 2,
-                  ),
-                  new Expanded(
-                    child: new Image(image: new AssetImage("assets/logo_splash.png")),
-                    flex: 2,
-                  ),
-                  new Expanded(
-                    child: new Container(
-                      child: null,
+                    new Expanded(
+                      child: new Image(image: new AssetImage("assets/logo_splash.png")),
+                      flex: 2,
                     ),
-                    flex: 2,
-                  ),
-                ],
+                    new Expanded(
+                      child: new Container(
+                        child: null,
+                      ),
+                      flex: 2,
+                    ),
+                  ],
+                ),
               ),
+              flex: 8,
             ),
-            flex: 8,
-          ),
-          new Expanded(
-            child: Form(
-              key: forkey,
-              child: Column(
-                children: [
-                  new Expanded(
-                    child: new Card(
+            new Expanded(
+              child: Form(
+                key: forkey,
+                child: Column(
+                  children: [
+                    new Expanded(
+                      child: new Card(
                         child : TextFormField(
                           decoration: InputDecoration(
                               hintText: "Usuario"
@@ -108,10 +108,10 @@ class _MyHomePageState extends State<MyHomePage> {
                           },
                           onSaved: (input) => _email = input,
                         ),
-                    ),
-                    flex: 1,
-                  ),new Expanded(
-                    child: new Card(
+                      ),
+                      flex: 1,
+                    ),new Expanded(
+                      child: new Card(
                         child : TextFormField(
                           decoration: InputDecoration(
                               hintText: "contraseña"
@@ -123,102 +123,322 @@ class _MyHomePageState extends State<MyHomePage> {
                           },
                           onSaved: (input) => _password = input,
                         ),
+                      ),
+                      flex: 1,
                     ),
-                    flex: 1,
-                  ),
-                ],
+                  ],
+                ),
               ),
+              flex: 2,
             ),
-            flex: 2,
-          ),
-          new Expanded(
-            child: new Container(
-              child: Column(
-                children: [
-                  new Expanded(
-                    child: new Card(
-                      child : null,
+            new Expanded(
+              child: new Container(
+                child: Column(
+                  children: [
+                    new Expanded(
+                      child: new Card(
+                        child : null,
+                      ),
+                      flex: 1,
                     ),
-                    flex: 1,
-                  ),
-                ],
+                  ],
+                ),
               ),
+              flex: 1,
             ),
-            flex: 1,
-          ),
-          new Expanded(
-            child: new Container(
-              child: Column(
-                children: [
-                  new Expanded(
-                    child: new Card(
-                      child : RaisedButton(
-                        textColor: Colors.white,
-                        color: Colors.blue,
-                        onPressed: () {
-                          if (forkey.currentState.validate()) {
-                            forkey.currentState.save();
-                            _GetLoginAndChangeView();
-                          }
-                        },
-                        child: const Text(
-                            'INGRESAR',
-                            style: TextStyle(fontSize: 13)
+            new Expanded(
+              child: new Container(
+                child: Column(
+                  children: [
+                    new Expanded(
+                      child: new Card(
+                        child : RaisedButton(
+                          textColor: Colors.white,
+                          color: Colors.blue,
+                          onPressed: () {
+                            if (forkey.currentState.validate()) {
+                              forkey.currentState.save();
+                              _GetLoginAndChangeView();
+                            }
+                          },
+                          child: const Text(
+                              'INGRESAR',
+                              style: TextStyle(fontSize: 13)
+                          ),
                         ),
                       ),
-                    ),
-                    flex: 1,
-                  ),new Expanded(
-                    child: new Card(
-                      child : RaisedButton(
-                        textColor: Colors.white,
-                        color: Colors.blue,
-                        onPressed: () {},
-                        child: const Text(
-                            'CREA TU CUENTA, AFILIATE AQUÍ',
-                            style: TextStyle(fontSize: 13)
+                      flex: 1,
+                    ),new Expanded(
+                      child: new Card(
+                        child : RaisedButton(
+                          textColor: Colors.white,
+                          color: Colors.blue,
+                          onPressed: () {},
+                          child: const Text(
+                              'CREA TU CUENTA, AFILIATE AQUÍ',
+                              style: TextStyle(fontSize: 13)
+                          ),
                         ),
                       ),
+                      flex: 1,
                     ),
-                    flex: 1,
-                  ),
-                ],
+                  ],
+                ),
               ),
+              flex: 2,
             ),
-            flex: 2,
-          ),
-          new Expanded(
-            child: new Container(
-              child: Column(
-                children: [
-                  new Expanded(
-                    child: new Card(
-                      child : null,
+            new Expanded(
+              child: new Container(
+                child: Column(
+                  children: [
+                    new Expanded(
+                      child: new Card(
+                        child : null,
+                      ),
+                      flex: 1,
                     ),
-                    flex: 1,
-                  ),
-                ],
+                  ],
+                ),
               ),
+              flex: 1,
             ),
-            flex: 1,
-          ),
-        ])
+          ])
       ),
     );
   }
 }
 
 class FirstRoute extends StatelessWidget {
+  final forkey = GlobalKey<FormState>();
+  String _email, _password;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          child: Text('Regresar'),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        image: DecorationImage(image: AssetImage("assets/splash_main.jpg"), fit: BoxFit.cover),
+      ),
+      child:  new Container(
+          child: Column(children: <Widget>[
+            new Expanded(
+              child: new Container(
+                child: Row(
+                  children: [
+                    new Expanded(
+                      child: Column(
+                          children:[
+                            new Expanded(
+                              child: new Container(
+                                child: new Image(
+                                    width :150,
+                                    height:150,
+                                    image: new AssetImage("assets/logo_splash.png")),
+                              ),
+                              flex: 5,
+                            ),
+                            new Expanded(
+                              child: new Container(
+                                child: new Material(
+                                color: Colors.transparent,
+                                  child : Text(
+                                    'SERGIO ELIZONDO',
+                                    textAlign: TextAlign.center,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(fontSize: 16,color: Colors.white),
+                                  ),
+                                )
+                              ),
+                              flex: 1,
+                            ),
+                            new Expanded(
+                              child: new Container(
+                                  child: new Material(
+                                    color: Colors.transparent,
+                                    child : Text(
+                                      '1.0',
+                                      textAlign: TextAlign.center,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: TextStyle(fontSize: 16,color: Colors.white),
+                                    ),
+                                  )
+                              ),
+                              flex: 1,
+                            ),
+                          ]),
+                      flex: 2,
+                    ),
+                  ],
+                ),
+              ),
+              flex: 4,
+            ),
+            new Expanded(
+              child: new Container(
+                child: Column(children: <Widget>[
+                  new Expanded(
+                    child: new Container(
+                      child: Row(
+                        children: [
+                          new Expanded(
+                            child: new GestureDetector(
+                              onTap: (){
+                                print("tapraptaptap");
+                              },
+                              child: new Container(
+                                margin: const EdgeInsets.fromLTRB(10.0,10.0,10.0,10),
+                                color: Colors.white,
+                                child: Column(
+                                  children: <Widget>[
+                                    new Expanded(
+                                      child: new Container(
+                                        child: new Image(
+                                            height: 100,
+                                            width: 100,
+                                            image: new AssetImage("assets/tareas.png")
+                                        ),
+                                      ),
+                                      flex: 2,
+                                    ),
+                                    new Expanded(
+                                      child: new Material(
+                                        child : Text(
+                                          'TAREAS',
+                                          textAlign: TextAlign.center,
+                                          overflow: TextOverflow.ellipsis,
+                                          style: TextStyle(fontSize: 16,color: Colors.grey),
+                                        ),
+                                      ),
+                                      flex: 1,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            flex: 2,
+                          ),
+                          new Expanded(
+                            child: new GestureDetector(
+                              onTap: (){
+                              print("tapraptaptap");
+                              },
+                              child: new Container(
+                              child: new Container(
+                                margin: const EdgeInsets.fromLTRB(10.0,10.0,10.0,10),
+                                color: Colors.white,
+                                child: Column(
+                                  children: <Widget>[
+                                    new Expanded(
+                                      child: new Container(
+                                        child: new Image(
+                                            height: 100,
+                                            width: 100,
+                                            image: new AssetImage("assets/solicitudes.png")
+                                        ),
+                                      ),
+                                      flex: 2,
+                                    ),
+                                    new Expanded(
+                                      child: new Material(
+                                        child : Text(
+                                          'SOLICITUDES',
+                                          textAlign: TextAlign.center,
+                                          overflow: TextOverflow.ellipsis,
+                                          style: TextStyle(fontSize: 16,color: Colors.grey),
+                                        ),
+                                      ),
+                                      flex: 1,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            ),
+                            flex: 2,
+                          ),
+                        ],
+                      ),
+                    ),
+                    flex: 4,
+                  ),
+                  new Expanded(
+                    child: new Container(
+                      child: new Container(
+                        child: Row(
+                          children: [
+                            new Expanded(
+                              child: new Container(
+                                margin: const EdgeInsets.fromLTRB(10.0,10.0,10.0,20),
+                                color: Colors.white,
+                                child: Column(
+                                  children: <Widget>[
+                                    new Expanded(
+                                      child: new Container(
+                                        child: new Image(
+                                            height: 100,
+                                            width: 100,
+                                            image: new AssetImage("assets/estado_cuenta.png")
+                                        ),
+                                      ),
+                                      flex: 2,
+                                    ),
+                                    new Expanded(
+                                      child: new Material(
+                                        child : Text(
+                                          'ESTADO DE CUENTA',
+                                          textAlign: TextAlign.center,
+                                          overflow: TextOverflow.ellipsis,
+                                          style: TextStyle(fontSize: 16,color: Colors.grey),
+                                        ),
+                                      ),
+                                      flex: 1,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              flex: 2,
+                            ),
+                            new Expanded(
+                              child: new Container(
+                                margin: const EdgeInsets.fromLTRB(10.0,10.0,10.0,20),
+                                color: Colors.white,
+                                child: Column(
+                                  children: <Widget>[
+                                    new Expanded(
+                                      child: new Container(
+                                        child: new Image(
+                                            height: 100,
+                                            width: 100,
+                                            image: new AssetImage("assets/promociones.png")
+                                        ),
+                                      ),
+                                      flex: 2,
+                                    ),
+                                    new Expanded(
+                                      child: new Material(
+                                        child : Text(
+                                          'PROMOCIONES',
+                                          textAlign: TextAlign.center,
+                                          overflow: TextOverflow.ellipsis,
+                                          style: TextStyle(fontSize: 16,color: Colors.grey),
+                                        ),
+                                      ),
+                                      flex: 1,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              flex: 2,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    flex: 4,
+                  ),
+                ]),
+              ),
+              flex: 6,
+            ),
+          ])
       ),
     );
   }
