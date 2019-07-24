2a2dbd54681bc74f6e2ef17db3ae7cdfdac976b3
Ricardo <car.ric.lop@gmail.com>
2019-07-24 14:49:57 -0500

    prueba

--- lib/main.dart
+++ lib/main.dart
@@ -45,16 +45,16 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  final forkey = GlobalKey<FormState>();
+  String _email, _password;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _GetLoginAndChangeView() {
+    print(_email);
+    print(_password);
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => FirstRoute()),
+    );
   }
 
   @override
@@ -63,15 +63,162 @@ class _MyHomePageState extends State<MyHomePage> {
       decoration: BoxDecoration(
         image: DecorationImage(image: AssetImage("assets/splash.jpg"), fit: BoxFit.cover),
       ),
-      child: Stack(children: <Widget>[
-        Positioned(
-          top: 20 ,
-          left: 90,
-          width: 200,
-          height: 200,
-          child: new Image(image: new AssetImage("assets/logo_splash.png")),
+      child:  new Container(
+        child: Column(children: <Widget>[
+          new Expanded(
+            child: new Container(
+              child: Row(
+                children: [
+                  new Expanded(
+                    child: new Container(
+                      child: null,
+                    ),
+                    flex: 2,
+                  ),
+                  new Expanded(
+                    child: new Image(image: new AssetImage("assets/logo_splash.png")),
+                    flex: 2,
+                  ),
+                  new Expanded(
+                    child: new Container(
+                      child: null,
+                    ),
+                    flex: 2,
+                  ),
+                ],
+              ),
+            ),
+            flex: 8,
+          ),
+          new Expanded(
+            child: Form(
+              key: forkey,
+              child: Column(
+                children: [
+                  new Expanded(
+                    child: new Card(
+                        child : TextFormField(
+                          decoration: InputDecoration(
+                              hintText: "Usuario"
+                          ),
+                          validator: (value) {
+                            if(value.isEmpty) {
+                              return 'Usuario vacio';
+                            }
+                          },
+                          onSaved: (input) => _email = input,
+                        ),
+                    ),
+                    flex: 1,
+                  ),new Expanded(
+                    child: new Card(
+                        child : TextFormField(
+                          decoration: InputDecoration(
+                              hintText: "contraseña"
+                          ),
+                          validator: (value) {
+                            if(value.isEmpty) {
+                              return 'Contraseña vacia';
+                            }
+                          },
+                          onSaved: (input) => _password = input,
+                        ),
+                    ),
+                    flex: 1,
+                  ),
+                ],
+              ),
+            ),
+            flex: 2,
+          ),
+          new Expanded(
+            child: new Container(
+              child: Column(
+                children: [
+                  new Expanded(
+                    child: new Card(
+                      child : null,
+                    ),
+                    flex: 1,
+                  ),
+                ],
+              ),
+            ),
+            flex: 1,
+          ),
+          new Expanded(
+            child: new Container(
+              child: Column(
+                children: [
+                  new Expanded(
+                    child: new Card(
+                      child : RaisedButton(
+                        textColor: Colors.white,
+                        color: Colors.blue,
+                        onPressed: () {
+                          if (forkey.currentState.validate()) {
+                            forkey.currentState.save();
+                            _GetLoginAndChangeView();
+                          }
+                        },
+                        child: const Text(
+                            'INGRESAR',
+                            style: TextStyle(fontSize: 13)
+                        ),
+                      ),
+                    ),
+                    flex: 1,
+                  ),new Expanded(
+                    child: new Card(
+                      child : RaisedButton(
+                        textColor: Colors.white,
+                        color: Colors.blue,
+                        onPressed: () {},
+                        child: const Text(
+                            'CREA TU CUENTA, AFILIATE AQUÍ',
+                            style: TextStyle(fontSize: 13)
+                        ),
+                      ),
+                    ),
+                    flex: 1,
+                  ),
+                ],
+              ),
+            ),
+            flex: 2,
+          ),
+          new Expanded(
+            child: new Container(
+              child: Column(
+                children: [
+                  new Expanded(
+                    child: new Card(
+                      child : null,
+                    ),
+                    flex: 1,
+                  ),
+                ],
+              ),
+            ),
+            flex: 1,
+          ),
+        ])
+      ),
+    );
+  }
+}
+
+class FirstRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(
+          child: Text('Regresar'),
+          onPressed: () {
+            Navigator.pop(context);
+          },
         ),
-      ],
       ),
     );
   }
