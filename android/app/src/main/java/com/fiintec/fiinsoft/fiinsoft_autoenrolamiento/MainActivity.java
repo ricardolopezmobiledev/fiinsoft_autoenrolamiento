f1f3d9479177962dc751e368787dcc718686d03e
Ricardo <car.ric.lop@gmail.com>
2019-07-24 10:24:06 -0500

    Initial commit

---
+++ android/app/src/main/java/com/fiintec/fiinsoft/fiinsoft_autoenrolamiento/MainActivity.java
@@ -0,0 +1,13 @@
+package com.fiintec.fiinsoft.fiinsoft_autoenrolamiento;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
